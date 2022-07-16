98e2fb386bda71e83a8a81d9e069a610c482cc66
Sonal Sithara <71545511+Sonal23520@users.noreply.github.com>
2022-07-16 18:05:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def fetch_poster(movie_id):
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distnaces = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distnaces)), reverse=True, key=lambda x: x[1])[1:6]
+    movies_list = sorted(list(enumerate(distnaces)), reverse=True, key=lambda x: x[1])[1:17]
 
     recommended_movies = []
     recommended_movies_posters = []
@@ -20,7 +20,7 @@ def recommend(movie):
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
         recommended_movies_posters.append(fetch_poster(movie_id))
-    return recommended_movies, recommended_movies_posters
+    return recommended_movies, recommended_movies_posters, 
 
 movies_dict = pickle.load(open('movie_dict.pkl','rb'))
 movies = pd.DataFrame(movies_dict)
@@ -28,18 +28,21 @@ ifile = bz2.BZ2File("similarity.pkl",'rb')
 similarity = pickle.load(ifile)
 ifile.close()
 
+
 st.title('Movie Recommendation System')
 
 selected_movie_name = st.selectbox(
     'Select One Movie',
     movies['title'].values
 )
+    
+
 
 if st.button('Recommend'):
-    names, posters = recommend(selected_movie_name)
-    col1, col2, col3, col4, col5 = st.columns(5)
+    names, posters  = recommend(selected_movie_name)
+    col1, col2, col3, col4, col5 , col6 , col7 ,col8 = st.columns(8)
     with col1:
-        st.text(names[0])
+        st.text(names[0])   
         st.image(posters[0])
     with col2:
         st.text(names[1])
@@ -53,3 +56,36 @@ if st.button('Recommend'):
     with col5:
         st.text(names[4])
         st.image(posters[4])
+    with col6:
+        st.text(names[5])
+        st.image(posters[5])
+    with col7:
+        st.text(names[6])
+        st.image(posters[6])
+    with col8:
+        st.text(names[7])
+        st.image(posters[7])
+    with col1:
+        st.text(names[8])   
+        st.image(posters[8])
+    with col2:
+        st.text(names[9])
+        st.image(posters[9])
+    with col3:
+        st.text(names[10])
+        st.image(posters[10])
+    with col4:
+        st.text(names[11])
+        st.image(posters[11])
+    with col5:
+        st.text(names[12])
+        st.image(posters[12])
+    with col6:
+        st.text(names[13])
+        st.image(posters[13])
+    with col7:
+        st.text(names[14])
+        st.image(posters[14])
+    with col8:
+        st.text(names[15])
+        st.image(posters[15])
